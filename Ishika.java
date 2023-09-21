884894deb3d04301925cae66c57d9e239c390723
Boltcrasher <Boltcrasher@gmail.com>
2023-09-21 13:33:10 -0500

    Create Ishika.java

---
+++ Ishika.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Ishika {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(new File("ishika.dat"));
+        int n = in.nextInt();
+        in.nextLine();
+        while (n-- > 0) {
+        }
+        in.close();
+    }
+}
